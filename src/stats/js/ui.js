6ffaa49531a984894bdbe4c2a86da9ae42736592
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2022-10-13 13:55:48 +0200

    stats gender/age

--- src/stats/js/ui.js
+++ src/stats/js/ui.js
@@ -1,26 +1,150 @@
 export { ui };
 
 class ui {
+	static labels;
 	static language;
 	static init() {
 		window.onresize = ui.resize;
-		var options = {
-			chart: {
-				type: 'line'
-			},
-			series: [{
-				name: 'sales',
-				data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-			}],
-			xaxis: {
-				categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-			}
-		}
-		var chart = new ApexCharts(ui.q("chart.no1"), options);
-		chart.render();
 		ui.language = (navigator.language || '').toLowerCase().indexOf('en') > -1 ? 'DE' : 'EN';
 		ui.setLanguage();
 		ui.resize();
+	}
+	static initCharts(data) {
+		ui.initChartGender(data.user);
+		ui.initChartAge(data.user);
+		ui.initChartLog(data.log);
+	}
+	static initChartGender(data) {
+		var index = {}, total = [0, 0, 0, 0], verified = [0, 0, 0, 0], withImage = [0, 0, 0, 0], genderMap = [2, 1, 3, null];
+		for (var i = 0; i < data[0].length; i++)
+			index[data[0][i]] = i;
+		for (var i = 1; i < data.length; i++) {
+			var x = data[i][index['_count']] / 1000;
+			for (var i2 = 0; i2 < genderMap.length; i2++) {
+				if (data[i][index['contact.gender']] == genderMap[i2]) {
+					total[i2] += x;
+					if (data[i][index['contact.verified']])
+						verified[i2] += x;
+					if (data[i][index['_image']])
+						withImage[i2] += x;
+				}
+			}
+		}
+		for (var i = 0; i < total.length; i++) {
+			total[i] = (parseInt(total[i] * 10 + 0.5) / 10);
+			verified[i] = (parseInt(verified[i] * 10 + 0.5) / 10);
+			withImage[i] = (parseInt(withImage[i] * 10 + 0.5) / 10);
+		}
+		new ApexCharts(ui.q("chart.gender"), {
+			chart: {
+				type: 'bar',
+				toolbar: {
+					show: false
+				}
+			},
+			plotOptions: {
+				bar: {
+					horizontal: true
+				}
+			},
+			dataLabels: {
+				enabled: true,
+				textAnchor: 'start',
+				formatter: function (val, opt) {
+					return val + '%'
+				},
+				offsetX: 0,
+			},
+			tooltip: {
+				y: {
+					formatter: function (value, { series, seriesIndex, dataPointIndex, w }) {
+						return value + '%';
+					}
+				}
+			},
+			series: [{
+				name: ui.labels['total'],
+				data: total
+			},
+			{
+				name: ui.labels['verified'],
+				data: verified
+			},
+			{
+				name: ui.labels['withImage'],
+				data: withImage
+			}],
+			labels: [ui.labels['female'], ui.labels['male'], ui.labels['divers'], ui.labels['noData']]
+		}).render();
+	}
+	static initChartAge(data) {
+		var index = {}, female = [0, 0, 0, 0, 0, 0, 0], male = [0, 0, 0, 0, 0, 0, 0], divers = [0, 0, 0, 0, 0, 0, 0], noData = [0, 0, 0, 0, 0, 0, 0], genderMap = [2, 1, 3, null];
+		for (var i = 0; i < data[0].length; i++)
+			index[data[0][i]] = i;
+		for (var i = 1; i < data.length; i++) {
+			var x = data[i][index['_count']] / 1000, i2;
+			if (data[i][index['_age']] == null)
+				i2 = male.length - 1;
+			else
+				i2 = data[i][index['_age']] - 2;
+			if (i2 < 0)
+				i2 = 0;
+			else if (i2 > male.length - 1)
+				i2 = male.length - 1;
+			if (data[i][index['contact.gender']] == 1)
+				male[i2] += x;
+			else if (data[i][index['contact.gender']] == 2)
+				female[i2] += x;
+			else if (data[i][index['contact.gender']] == 3)
+				divers[i2] += x;
+			else
+				noData[i2] += x;
+		}
+		for (var i = 0; i < female.length; i++) {
+			female[i] = parseInt(0.5 + female[i]);
+			male[i] = parseInt(0.5 + male[i]);
+			divers[i] = parseInt(0.5 + divers[i]);
+			noData[i] = parseInt(0.5 + noData[i]);
+		}
+		new ApexCharts(ui.q("chart.age"), {
+			chart: {
+				type: 'bar',
+				toolbar: {
+					show: false
+				}
+			},
+			dataLabels: {
+				formatter: function (val, opt) {
+					return val + '%'
+				}
+			},
+			tooltip: {
+				y: {
+					formatter: function (value, { series, seriesIndex, dataPointIndex, w }) {
+						return value + '%';
+					}
+				}
+			},
+			series: [{
+				name: ui.labels['female'],
+				data: female
+			},
+			{
+				name: ui.labels['male'],
+				data: male
+			},
+			{
+				name: ui.labels['divers'],
+				data: divers
+			},
+			{
+				name: ui.labels['noData'],
+				data: noData
+			}],
+			labels: [ui.labels['until'] + ' 20', '20-30', '30-40', '40-50', '50-60', ui.labels['from'] + ' 60', ui.labels['noData']]
+		}).render();
+	}
+	static initChartLog(data) {
 	}
 	static q(path) {
 		return document.querySelector(path);
@@ -37,19 +161,19 @@ class ui {
 		xmlhttp.onreadystatechange = function () {
 			if (xmlhttp.readyState == 4) {
 				if (xmlhttp.status >= 200 && xmlhttp.status < 300) {
-					var e = ui.qa('[l]'), labels = JSON.parse(xmlhttp.responseText), id, s;
-					for (var i = 0; i < e.length; i++) {
-						id = e[i].getAttribute('l');
-						s = labels[id];
-						if (!s && id.indexOf('.') > 0) {
-							var s2 = id.split('.');
-							if (labels[s2[0]])
-								s = labels[s2[0]][s2[1]];
-						}
-						e[i].innerHTML = s + (e[i].nodeName == 'BOX' ? '<cover></cover>' : '');
-					}
+					var e = ui.qa('[l]'), id, s;
+					ui.labels = JSON.parse(xmlhttp.responseText);
+					for (var i = 0; i < e.length; i++)
+						e[i].innerHTML = ui.labels[e[i].getAttribute('l')] + (e[i].nodeName == 'BOX' ? '<cover></cover>' : '');
 					ui.language = language;
 					ui.q('language').innerHTML = language == 'DE' ? 'EN' : 'DE';
+					xmlhttp = new XMLHttpRequest();
+					xmlhttp.onreadystatechange = function () {
+						if (xmlhttp.readyState == 4 && xmlhttp.status >= 200 && xmlhttp.status < 300)
+							ui.initCharts(JSON.parse(xmlhttp.responseText));
+					};
+					xmlhttp.open('GET', 'statistics.json', true);
+					xmlhttp.send();
 				}
 			}
 		};
